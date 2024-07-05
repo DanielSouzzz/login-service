ebc28f4713290e6095f6b31404d1ddc2b22dfaf4
daniel <danielsouzanovo@gmail.com>
2024-07-04 23:30:35 -0300

    created annotation wich lombok.

--- backend/projeto (2)/projeto/src/main/java/br/com/agendapro/projeto/model/User.java
+++ backend/projeto (2)/projeto/src/main/java/br/com/agendapro/projeto/model/User.java
@@ -1,6 +1,9 @@
 package br.com.agendapro.projeto.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
 
 @Entity
 @Table(name = "Users")
@@ -11,56 +14,15 @@ public class User {
     @Column(name = "userId") // Especifica o nome exato da coluna no banco de dados
     private Integer id;
 
-    @Column(name = "username", length = 200, nullable = true)
+    @Column(name = "username", length = 200, nullable = false)
     private String username;
 
-    @Column(name = "email", length = 200, nullable = true)
+    @Column(name = "email", length = 200, nullable = false)
     private String email;
 
-    @Column(name = "password", columnDefinition = "TEXT", nullable = true)
+    @Column(name = "password", columnDefinition = "TEXT", nullable = false)
     private String password;
 
-    @Column(name = "phone", length = 15, nullable = true)
+    @Column(name = "phone", length = 15, nullable = false)
     private String phone;
-
-    // Getters e Setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
